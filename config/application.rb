b2406b6d4ccc28873d9d899324641027740b7a00
haleymnatz <haleymnatz@gmail.com>
2017-06-13 21:37:29 -0400

    Add code to application.rb for CORS

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,9 @@ module Notes
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
+    'Access-Control-Request-Method' => %w{GET POST DELETE UPDATE OPTIONS}.join(",")
+    }
   end
 end
